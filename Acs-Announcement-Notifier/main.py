55511d09ce9cf7ce29a9eda14316105a12b695f2
dokma11 <vule.dok@gmail.com>
2024-02-22 11:58:01 +0100

    feat: IIS scraper implementation

--- Acs-Announcement-Notifier/main.py
+++ Acs-Announcement-Notifier/main.py
@@ -1,5 +1,4 @@
 from email.mime.text import MIMEText
-
 import requests
 import bs4
 import smtplib
@@ -55,6 +54,56 @@ def scrape_sbp():
         print('Error:', response.status_code)
 
 
+def scrape_iis():
+    iis_url = 'http://www.acs.uns.ac.rs/sr/ism'
+    iis_last_h2 = 'Raspored polaganja usmenog ispita kod prof. Slavica Kordić za 21.2.2024.'
+    iis_last_em = 'Baze podataka 1 - Informacioni inženjering - 	20. Februar 2024'
+    iis_last_p = ''
+
+    response = requests.get(iis_url)
+
+    if response.status_code == 200:
+        soup = bs4.BeautifulSoup(response.text, 'html.parser')
+        content_div = soup.find('div', class_='view-content')
+
+        if content_div:
+            p_tag = content_div.find('p')
+            h2_tag = content_div.find('h2')
+            em_tag = content_div.find('em')
+
+            if h2_tag:
+                print(h2_tag.get_text(strip=True))
+            else:
+                print('No <h2> tag found within the <div>')
+
+            if em_tag:
+                # Radi prikaza samo treba fromatirati, ako bude i bilo neophodno da se bilo sta prikaze
+                print(em_tag.get_text(strip=True))
+            else:
+                print('No <em> tag found within the <div>')
+
+            if p_tag:
+                print(p_tag.get_text(strip=True))
+            else:
+                print('No <p> tag found within the <div>')
+            # Pitanje je da li treba i prilog recimo dodati, mozda da se pristupi direktno sa mejla?
+
+            if (h2_tag.get_text(strip=True) != iis_last_h2 or em_tag.get_text(strip=True) != iis_last_em or
+                    p_tag.get_text(strip=True) != iis_last_p):
+                email_subject = 'ACS-Announcement'
+                email_body = f'{h2_tag.get_text(strip=True)}<br>{em_tag.get_text(strip=True)}<br><br>{p_tag.get_text(strip=True)}'
+                send_email(email_subject, email_body.encode('utf-8'))
+                sbp_last_h2 = h2_tag.get_text(strip=True)
+                sbp_last_em = em_tag.get_text(strip=True)
+                sbp_last_p = p_tag.get_text(strip=True)
+            else:
+                print('There are no new announcements')
+        else:
+            print('No <div> found')
+    else:
+        print('Error:', response.status_code)
+
+
 def send_email(subject, body):
     msg = MIMEText(body, 'html', 'utf-8')
 
@@ -69,4 +118,5 @@ def send_email(subject, body):
 
 
 if __name__ == "__main__":
-    scrape_sbp()
+    #scrape_sbp()
+    scrape_iis()
