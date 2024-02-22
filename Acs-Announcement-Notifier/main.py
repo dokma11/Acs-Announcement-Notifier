53309b3c5bc94ba5197dbe65d9ef2383cb3db9da
dokma11 <vule.dok@gmail.com>
2024-02-22 12:05:23 +0100

    refactor: Response format

--- Acs-Announcement-Notifier/main.py
+++ Acs-Announcement-Notifier/main.py
@@ -13,43 +13,7 @@ def scrape_sbp():
     response = requests.get(sbp_url)
 
     if response.status_code == 200:
-        soup = bs4.BeautifulSoup(response.text, 'html.parser')
-        content_div = soup.find('div', class_='view-content')
-
-        if content_div:
-            p_tag = content_div.find('p')
-            h2_tag = content_div.find('h2')
-            em_tag = content_div.find('em')
-
-            if h2_tag:
-                print(h2_tag.get_text(strip=True))
-            else:
-                print('No <h2> tag found within the <div>')
-
-            if em_tag:
-                # Radi prikaza samo treba fromatirati, ako bude i bilo neophodno da se bilo sta prikaze
-                print(em_tag.get_text(strip=True))
-            else:
-                print('No <em> tag found within the <div>')
-
-            if p_tag:
-                print(p_tag.get_text(strip=True))
-            else:
-                print('No <p> tag found within the <div>')
-            # Pitanje je da li treba i prilog recimo dodati, mozda da se pristupi direktno sa mejla?
-
-            if (h2_tag.get_text(strip=True) != sbp_last_h2 or em_tag.get_text(strip=True) != sbp_last_em or
-                    p_tag.get_text(strip=True) != sbp_last_p):
-                email_subject = 'ACS-Announcement'
-                email_body = f'{h2_tag.get_text(strip=True)}<br>{em_tag.get_text(strip=True)}<br><br>{p_tag.get_text(strip=True)}'
-                send_email(email_subject, email_body.encode('utf-8'))
-                sbp_last_h2 = h2_tag.get_text(strip=True)
-                sbp_last_em = em_tag.get_text(strip=True)
-                sbp_last_p = p_tag.get_text(strip=True)
-            else:
-                print('There are no new announcements')
-        else:
-            print('No <div> found')
+        sbp_last_h2, sbp_last_em, sbp_last_p = format_response(response, sbp_last_h2, sbp_last_em, sbp_last_p)
     else:
         print('Error:', response.status_code)
 
@@ -63,45 +27,48 @@ def scrape_iis():
     response = requests.get(iis_url)
 
     if response.status_code == 200:
-        soup = bs4.BeautifulSoup(response.text, 'html.parser')
-        content_div = soup.find('div', class_='view-content')
-
-        if content_div:
-            p_tag = content_div.find('p')
-            h2_tag = content_div.find('h2')
-            em_tag = content_div.find('em')
-
-            if h2_tag:
-                print(h2_tag.get_text(strip=True))
-            else:
-                print('No <h2> tag found within the <div>')
-
-            if em_tag:
-                # Radi prikaza samo treba fromatirati, ako bude i bilo neophodno da se bilo sta prikaze
-                print(em_tag.get_text(strip=True))
-            else:
-                print('No <em> tag found within the <div>')
-
-            if p_tag:
-                print(p_tag.get_text(strip=True))
-            else:
-                print('No <p> tag found within the <div>')
-            # Pitanje je da li treba i prilog recimo dodati, mozda da se pristupi direktno sa mejla?
-
-            if (h2_tag.get_text(strip=True) != iis_last_h2 or em_tag.get_text(strip=True) != iis_last_em or
-                    p_tag.get_text(strip=True) != iis_last_p):
-                email_subject = 'ACS-Announcement'
-                email_body = f'{h2_tag.get_text(strip=True)}<br>{em_tag.get_text(strip=True)}<br><br>{p_tag.get_text(strip=True)}'
-                send_email(email_subject, email_body.encode('utf-8'))
-                sbp_last_h2 = h2_tag.get_text(strip=True)
-                sbp_last_em = em_tag.get_text(strip=True)
-                sbp_last_p = p_tag.get_text(strip=True)
-            else:
-                print('There are no new announcements')
-        else:
-            print('No <div> found')
+        iis_last_h2, iis_last_em, iis_last_p = format_response(response, iis_last_h2, iis_last_em, iis_last_p)
     else:
         print('Error:', response.status_code)
+
+
+def format_response(response, last_h2, last_em, last_p):
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+    content_div = soup.find('div', class_='view-content')
+
+    if content_div:
+        p_tag = content_div.find('p')
+        h2_tag = content_div.find('h2')
+        em_tag = content_div.find('em')
+
+        if h2_tag:
+            print(h2_tag.get_text(strip=True))
+        else:
+            print('No <h2> tag found within the <div>')
+
+        if em_tag:
+            # Radi prikaza samo treba fromatirati, ako bude i bilo neophodno da se bilo sta prikaze
+            print(em_tag.get_text(strip=True))
+        else:
+            print('No <em> tag found within the <div>')
+
+        if p_tag:
+            print(p_tag.get_text(strip=True))
+        else:
+            print('No <p> tag found within the <div>')
+        # Pitanje je da li treba i prilog recimo dodati, mozda da se pristupi direktno sa mejla?
+
+        if (h2_tag.get_text(strip=True) != last_h2 or em_tag.get_text(strip=True) != last_em or
+                p_tag.get_text(strip=True) != last_p):
+            email_subject = 'ACS-Announcement'
+            email_body = f'{h2_tag.get_text(strip=True)}<br>{em_tag.get_text(strip=True)}<br><br>{p_tag.get_text(strip=True)}'
+            send_email(email_subject, email_body.encode('utf-8'))
+            print('Email sent')
+            return h2_tag.get_text(strip=True), em_tag.get_text(strip=True), p_tag.get_text(strip=True)
+        else:
+            print('There are no new announcements')
+    else:
+        print('No <div> found')
 
 
 def send_email(subject, body):
